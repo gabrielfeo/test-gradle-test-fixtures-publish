856ca190b66acd8737e15eac272ea54572f5b640
Gabriel Feo <gabriel@gabrielfeo.com>
2023-05-11 18:41:54 +0100

    Simplify sample

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,23 +1,13 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.8.21"
-    id("org.jetbrains.dokka") version "1.8.10"
-    id("org.openapi.generator") version "6.5.0"
     `java-test-fixtures`
     `maven-publish`
 }
 
 group = "com.github.gabrielfeo"
 
-java {
-    withSourcesJar()
-    withJavadocJar()
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
-    }
-}
-
-tasks.named<Jar>("javadocJar") {
-    from(tasks.dokkaHtml)
+repositories {
+    mavenCentral()
 }
 
 publishing {
@@ -26,17 +16,5 @@ publishing {
             artifactId = "library"
             from(components["java"])
         }
-    }
-}
-
-testing {
-    suites.withType<JvmTestSuite> {
-        useKotlinTest()
-    }
-}
-
-java {
-    consistentResolution {
-        useRuntimeClasspathVersions()
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,5 @@
 rootProject.name = "root"
 
-dependencyResolutionManagement {
-    repositories {
-        mavenCentral()
-    }
-}
-
 include(
     ":library",
     ":sample",
